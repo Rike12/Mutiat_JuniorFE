213038c2347f4cd3486a40dd56b2dad93a6a4496
mutiat <mutiat900@gmail.com>
2024-04-23 23:20:53 +0100

    Created the banner section

---
+++ spacex-data-app/src/Banner.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import rocketImage from './assets/rocket.png';
+
+const Banner = () => {
+  return (
+    <div className="bg-gray-900 text-white py-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-center">
+        
+        {/* Text Content */}
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">Explore SpaceX Rockets</h1>
+          <p className="text-lg mb-8">Discover the latest rockets launched by SpaceX and explore their missions.</p>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full">Learn More</button>
+        </div>
+
+        {/* Rocket Image */}
+        <div className="mr-10">
+          <img src={rocketImage} alt="Rocket" className="w-74 h-70" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- spacex-data-app/src/Navbar.tsx
+++ spacex-data-app/src/Navbar.tsx
@@ -9,11 +9,11 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-500 shadow-lg">
+    <nav className="bg-gray-900 text-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-          <img src={Logo} alt="Logo"className="object-cover object-center w-10 h-10" />
+          <img src={Logo} alt="Logo"className="object-cover object-center w-60 h-10" />
           </div>
 
           {/* Hamburger Menu */}
